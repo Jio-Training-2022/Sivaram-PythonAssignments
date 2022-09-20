f88bb180e3ea696b0ec4028d274cff23433507b7
imshivaram <113836409+imshivaram@users.noreply.github.com>
2022-09-20 22:58:17 +0530

    Update Problems4.py

--- Day4/Problems4.py
+++ Day4/Problems4.py
@@ -1,4 +1,4 @@
-# For any given integer array A ( read as input by the user ). How will you find the second largest element/value in the array? Write code for the same.
+# 1) For any given integer array A ( read as input by the user ). How will you find the second largest element/value in the array? Write code for the same.
 
 import math
 arr = [34, 67, 12, 78, 54, 89, 15, 56]
@@ -25,7 +25,7 @@ print(slargest)
 78
 '''
 
-# Given an array nums ( read as input by the user), separate the odd and even numbers from the array and print them on separate lines.
+# 2) Given an array nums ( read as input by the user), separate the odd and even numbers from the array and print them on separate lines.
 
 arr = [34, 67, 12, 78, 54, 89, 15, 56]
 # arr=[]       #if you want to enter the array elements from user
@@ -47,3 +47,17 @@ print(arr2)
 [34, 12, 78, 54, 56]
 [67, 89, 15]
 '''
+
+# 3) Given a number A ( read as input ), find the sum of its digits.
+def add_num(a,b):
+        sum1=a+b
+        print("Sum of two nos :", sum1)
+a = int(input("First number :"))
+b = int(input("Second number :"))
+  
+add_num(a,b)
+
+'''output
+First number :5
+Second number :8
+Sum of two nos :13'''

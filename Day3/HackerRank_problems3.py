ba58f0e402a15c56bc68b56774a7e360aa873240
imshivaram <113836409+imshivaram@users.noreply.github.com>
2022-09-20 21:11:14 +0530

    Create HackerRank_problems3.py

---
+++ Day3/HackerRank_problems3.py
@@ -0,0 +1,26 @@
+# Hackerrank problems:
+
+# Problem 1) The provided code stub reads and integer,n , from STDIN. For all non-negative integers 
+i<n, print i^2
+n = int(input())
+    for i in range(n):
+        print (i * i)
+
+# Problem 2) Given an integer,n , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2  to 5 , print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20, print Not Weird
+
+if N % 2 != 0:
+  print("Weird")
+else:
+    if N >= 2 and N <= 5:
+      print("Not Weird")
+    elif N >= 6 and N <= 20:
+      print("Weird")
+    elif N > 20:
+      print("Not Weird")
+ 
+ 

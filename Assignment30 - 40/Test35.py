4ae8f4d16708c7429b9760d8fb6b98cf2fc9a972
imshivaram <113836409+imshivaram@users.noreply.github.com>
2022-10-16 01:36:45 +0530

    Delete Test35.py

--- Assignment30 - 40/Test35.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-  
-Row = int(input("Enter the number of rows:"))
-Column = int(input("Enter the number of columns:"))
-print("Enter the entries in a single line (separated by space): ")
-entries = list(map(int, input().split()))
-matrix = np.array(entries).reshape(Row, Column)
-print(matrix)

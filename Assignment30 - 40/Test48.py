37f78e4aca4a3504e0af308e64a526226b8f379b
imshivaram <113836409+imshivaram@users.noreply.github.com>
2022-10-16 01:37:24 +0530

    Delete Test48.py

--- Assignment30 - 40/Test48.py
+++
@@ -1,8 +0,0 @@
-from os import walk
-
-f = []
-for (dirpath, dirnames, filenames) in walk(r'C:\Users\sivar\OneDrive\Desktop\Course\Python\Sample'):
-    f.extend(filenames)
-    break
-    
-print(f)
